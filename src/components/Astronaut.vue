eb5af9fe1f0c743827113990c444aa106db8612c
antoine <antoine.reib3l@gmail.com>
2024-11-02 23:23:27 +0100

    innitial commit

---
+++ src/components/Astronaut.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useMouse } from '@vueuse/core';
+const { x, y } = useMouse()
+
+</script>
+
+<template>
+    <span id="rotateMe">🧑‍🚀 {{ x }}  {{ y }}</span>
+</template>
+
+<style scoped>
+#rotateMe {
+    transform-origin: center center;
+    transition: transform 0.1s
+}
+</style>
--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
+const props = defineProps<{
+  initial?: number
+}>()
+const counter = ref(props.initial ?? 0)
 </script>
 
 <template>
-  $END$
+  <slot name="top"></slot>
+  <button @click="counter++">{{ counter }}</button>
+  <slot name="bottom"></slot>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
